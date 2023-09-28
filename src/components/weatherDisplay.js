5fe30fcb85c4e9f5aef791528f47f4033ee633cc
unknown <eugeneOduor79@gmail.com>
2023-09-28 23:12:18 +0300

    Create component for displaying location and temperature

---
+++ src/components/weatherDisplay.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { MdAccessAlarm, MdLocationOn } from 'react-icons/md';
+import { selectWeatherData } from '../redux/HomeSlice';
+import HourlyForecast from './HourlyForecast';
+import Details from './Details';
+import DailyForecastTemp from './DailyForecastTemp';
+import '../styles/WeatherDisplay.css';
+import DailyForecastPrecip from './DailyForecastPrecip';
+import Wind from './Wind';
+
+const WeatherDisplay = () => {
+  const weatherData = useSelector(selectWeatherData);
+
+  if (!weatherData) return null;
+
+  const { location, current } = weatherData;
+
+  return (
+    <div className="location-details">
+      <div className="summary">
+        <p className="now">Now</p>
+        <div className="condition">
+          <div className="tm">
+            <p className="summary-temperature">
+              {current.temp_c}
+              °C
+            </p>
+            <img alt="weather icon" src={current.condition.icon} />
+          </div>
+          <p>
+            {current.condition.text}
+          </p>
+        </div>
+        <hr className="horizontal-line" />
+        <div className="time-location">
+          <div className="location">
+            <MdLocationOn className="summary-icon" />
+            <p>
+              {location.name}
+              ,
+              {' '}
+              {location.country}
+            </p>
+          </div>
+          <div className="date">
+            <MdAccessAlarm className="summary-icon" />
+            <p>{location.localtime}</p>
+          </div>
+        </div>
+      </div>
+      <Details />
+      <HourlyForecast />
+      <DailyForecastTemp />
+      <DailyForecastPrecip />
+      <Wind />
+    </div>
+  );
+};
+
+export default WeatherDisplay;
