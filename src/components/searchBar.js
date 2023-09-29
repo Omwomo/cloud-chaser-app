f178037377849774a41c920e8950a0f04bcca2f2
unknown <eugeneOduor79@gmail.com>
2023-09-29 14:45:09 +0300

    Created search bar componet

---
+++ src/components/searchBar.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { HiSearch } from 'react-icons/hi';
+
+const SearchBar = ({ onSearch }) => {
+  const [query, setQuery] = useState('');
+
+  const handleSearch = () => {
+    if (query.trim()) {
+      onSearch(query);
+    }
+  };
+
+  return (
+    <div className="search-area">
+      <input
+        className="search-input"
+        placeholder="type location"
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <button
+        className="search-button"
+        type="button"
+        onClick={handleSearch}
+      >
+        <HiSearch />
+      </button>
+    </div>
+  );
+};
+
+SearchBar.propTypes = {
+  onSearch: PropTypes.func.isRequired,
+};
+
+export default SearchBar;
